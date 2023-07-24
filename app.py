2d7829e7c392fe7883473549769067f76a234f46
JarousNemec <jarous.nemec@gmail.com>
2023-07-24 18:30:50 +0200

    Revert "investigation a structure of azure web service project"
    This reverts commit 5bdae9649719768d49171e5bbc23fc55b61a619e.

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from soure_root.server import server
+from soure_root.logging.galg_logger import init_logger
+
+init_logger()
 
 
 def start_server():
--- soure_root/server.py
+++ soure_root/server.py
@@ -2,18 +2,18 @@ import os
 from flask import Flask, send_from_directory, Response, render_template, request, redirect, url_for
 from waitress import serve
 
+import logging
+
 
 def flask_app():
     app = Flask(__name__)
 
     @app.route("/")
     def client():
-        print("default...............................")
         return "Hello world"
 
     @app.route('/api/ping')
     def ping():
-        print("Ping...............................")
         return Response(status=200)
 
     return app
@@ -21,5 +21,5 @@ def flask_app():
 
 def server():
     manager_app = flask_app()
-    print("Started...............................")
+    logging.info("Serving on http://localhost:80")
     serve(manager_app, port=80)
